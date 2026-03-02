df8bbd61c335e405c8d7af83993985ab3d237b8c
nellyngz95 <118901404+nellyngz95@users.noreply.github.com>
2026-03-02 11:49:51 +0000

    Add files via upload

---
+++ Filters/HighAndLowPassTemplate/Templates/NoDSP/PluginProcessor.h
@@ -0,0 +1,70 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+/**
+*/
+class FiltersLabAudioProcessor  : public juce::AudioProcessor
+{
+public:
+    //==============================================================================
+    FiltersLabAudioProcessor();
+    ~FiltersLabAudioProcessor() override;
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    //==============================================================================
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+    //Declare the createParameterLayout() and your parameters object from juce::AudioProcessorValueTreeState.
+
+    //Loading a file
+    juce::AudioFormatManager formatManager;
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+    juce::AudioTransportSource transportSource;
+    void loadFile(const juce::File& file);
+    //Filters- Declare the highFilters
+    juce::OwnedArray<juce::IIRFilter> lowFilters;
+    
+    //DSP
+
+private:
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (FiltersLabAudioProcessor)
+};
